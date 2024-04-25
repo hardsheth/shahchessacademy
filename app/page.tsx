4ff8a33eaa43bac904d0786d3ac6054c0e22922f
hardsheth <hardsheth17@gmail.com>
2024-04-25 22:14:37 +0530

    Update in web view

--- app/page.tsx
+++ app/page.tsx
@@ -14,20 +14,17 @@ export default function Home() {
         <div className={`mx-5 ${Styles.textContainerNg}`}>
           <h3 className="pt-3 text-center">Play, Learn and Connect </h3>
           <p className="mt-3 pb-2 text-center flex-wrap px-5">
-            Chess Academy where passion meats strategy.<br /> A vibrant community of chess enthusiasts where you have expert coaches for professional guidance.<br /> Learn with Fun.
+            Chess Academy where passion meets strategy.<br /> A vibrant community of chess enthusiasts where you have expert coaches for professional guidance.<br /> Learn with Fun.
           </p>
         </div>
-        <div className="container px-6">
+        <div className="container">
           <div className="text-center w-100 ">
             <img src='/asset/vrushtiArchievement123.jpg' alt='Image will Come' className={`img-fluid  h-400 borderImage`} />
           </div>
-        </div>
-
-              <h3 className="text-center my-2">Our Courses</h3>
-        <div className="container px-6">
+          <h3 className="text-center my-2">Our Courses</h3>
           <div className={`row row-cols-1 row-cols-lg-4 gap-0 `}>
-            <div className="col col-lg-9 mx-0 ">
-            <ul className={`list-group list-group-flush mx-0-rem`}>
+            <div className="col col-lg-9 pe-0">
+              <ul className={`list-group list-group-flush `}>
                 <li className="list-group-item conatinerBackgrounds">Level 1 Beginner </li>
                 <li className="list-group-item conatinerBackgrounds">Level 2 Intermidiate-1</li>
                 <li className="list-group-item conatinerBackgrounds">Level 3 Intermidiate-2</li>
@@ -35,31 +32,24 @@ export default function Home() {
                 <li className="list-group-item conatinerBackgrounds">Extra Perks</li>
               </ul>
             </div>
-            <div className="col m-0">
-            <img src="/asset/vrushtiCources.jpg" alt='Image will Cource Image' className={`borderImage ${Styles.courceimgcontainer}`} />
+            <div className="col ps-0 pe-4">
+              <img src="/asset/vrushtiCourses.jpg" alt='Image will Cource Image' className={`borderImage ${Styles.courceimgcontainer}`} />
             </div>
-           
+
           </div>
+          <h3 className="text-center my-2">What We Offer</h3>
           <div className={`row mt-2 row-cols-1 row-cols-lg-4 w-100 ${Styles.containerHeight} text-align-center`}>
-            <div className="col  mx-0-rem m-0">
-              <img src="/asset/vrushtiCources.jpg" alt='Image will Cource Image' className={`borderImage ${Styles.imagecontainerHeight}`} />
+            <div className="col m-0 pe-0">
+              <img src="/asset/vrushtiCourses.png" alt='Image will Cource Image' className={`borderImage ${Styles.offer}`} />
             </div>
-            <div className="col col-lg-9 m-0">
-              <ul className="list-group list-group-flush conatinerBackgrounds mx-0-rem">
-                <li className="list-group-item conatinerBackgrounds">Opening Strategies</li>
-                <li className="list-group-item conatinerBackgrounds">Middle Game Mastery</li>
-                <li className="list-group-item conatinerBackgrounds">End Game Excellence</li>
-                <li className="list-group-item conatinerBackgrounds">Practical Application.</li>
+            <div className="col col-lg-9 p-0">
+              <ul className="list-group list-group-flush conatinerBackgrounds ">
+                <li className="list-group-item conatinerBackgrounds text-end">Opening Strategies</li>
+                <li className="list-group-item conatinerBackgrounds text-end">Middle Game Mastery</li>
+                <li className="list-group-item conatinerBackgrounds text-end">End Game Excellence</li>
+                <li className="list-group-item conatinerBackgrounds text-end">Practical Application.</li>
+                <li className="list-group-item conatinerBackgrounds text-end">Personal Attention.</li>
               </ul>
-            </div>
-          </div>
-          <div className={`row w-100 position-relative ${Styles.containerHeight} text-center mx-auto`}>
-            <div className="mx-auto">
-              <img src='/asset/vrushtiaward.jpg' alt='Image will Come' className={`borderImage img-fluid ${Styles.archivementImg}`} />
-            </div>
-            <div className="row ">
-              <h4 className="text-center">Location</h4>
-              <p className="text-center fs-4">Paldi, Ahmedabad</p>
             </div>
           </div>
           <div className={`row mb-3 ${Styles.containerHeight}`}>
@@ -72,17 +62,27 @@ export default function Home() {
                 <li className="list-group-item conatinerBackgrounds">Personal Attention</li>
               </ul>
             </div>
-            <div className="col-12 col-lg-4">
+            <div className="col-12 col-lg-4 mx-auto ">
               <button className="w-100 btn btn-primary ">Join our Live Chess classes now</button>
             </div>
           </div>
-          <div className={`row mb-2 row-cols-1 row-cols-lg-4 ${Styles.containerHeight} gap-0 h-auto`}>
-            <div className="col m-0 p-0">
-              <img src='/asset/vrushtiimage.png' className="img-fluid borderImage  maxpersonheight" alt='Image will Come' />
+          <div className={`row w-100 position-relative ${Styles.containerHeight} text-center mx-auto`}>
+            <div className="mx-auto">
+              <img src='/asset/vrushtiaward.jpg' alt='Image will Come' className={`borderImage img-fluid ${Styles.archivementImg}`} />
             </div>
-            <div className={`col col-lg-9 m-0 p-0 ${Styles.textContainerNg}`}>
+            <div className="row ">
+              <h4 className="text-center">Location</h4>
+              <p className="text-center fs-4">Paldi, Ahmedabad</p>
+            </div>
+          </div>
+         
+          <div className={`row mb-2 ms-1 row-cols-1 row-cols-lg-4 ${Styles.containerHeight} gap-0 h-auto bg-primary`}>
+          <div className="col m-0 p-0">
+              <img src='/asset/vrushtiimage.jpg' className={`img-fluid ${Styles.borderPerson} maxpersonheight`} alt='Image will Come' />
+            </div>
+            <div className={`col col-lg-9 m-0 p-0 pe-4 ${Styles.textContainerNg}`}>
               <h4 className="text-center my-3">About Vrushti Shah</h4>
-              <p className="text-start fs-6">Shah Vrushti, a seasoned chess player with over 15 years of experience and a highest FIDE rating of 1614. Multiple time district and state champion, received bronze medal at JITO Nationals from Bhuvneshwar Kumar. </p>
+              <p className="text-start fs-6">Shah Vrushti, a seasoned chess player with over 15 years of experience and a FIDE rating of 1650. Multiple time district and state champion, received bronze medal at JITO Nationals from Bhuvneshwar Kumar. </p>
               <p className="fs-6">
                 Received award for chess from Prime Minister Shri Narendra Modi, prestigious awards like Junior Jaydeep singhji Award and Shaktidutt Award.
               </p>
@@ -90,7 +90,7 @@ export default function Home() {
                 Played at International level in Thailand as well as participated in many National, Common Wealth, Asian tournaments.</p>
             </div>
           </div>
-          <div className={`row row-cols-1 row-cols-lg-3 ${Styles.containerHeight}`}>
+          <div className={`mt-5 row row-cols-1 row-cols-lg-3 ${Styles.containerHeight}`}>
             <div className="col">
               <h3 className="text-center">Contact Us</h3>
               <p className="">Ask us anything! We’re here to answer any questions you have.
@@ -110,6 +110,9 @@ export default function Home() {
               </p>
             </div>
             <div className="col">
+              <div className="mx-auto text-center">
+              <button type="button" className="btn btn-primary ">Book A Free Demo</button>
+            </div>
               <h3 className="text-center">Follow Us</h3>
               <div className="text-center">
                 <FaInstagram size={40} color="pink " className="mx-3" />
@@ -126,6 +129,8 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+
         {/* <p className="fs-4 text-center">Let's Improve together!</p> */}
       </div>
     </main>
